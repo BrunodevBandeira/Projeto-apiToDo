180710c8b5b8cc41e8cd72d2dac5cf6aca2c9d35
Bruno Bandeira <brunodev29ab@gmail.com>
2021-05-24 18:55:48 -0300

    Adicionando os importes necessários

--- angulaaar/frontEnd/src/app/app.component.ts
+++ angulaaar/frontEnd/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";//O FormGroup cria  um objeto que representa os campos dos formularios
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontEnd';
+  form: FormGroup = new FormGroup({
+    description: new FormControl("")//OBS: como é um objeto JavaScript, você coloca :
+  })
+
+  submit(){
+    console.log(this.form.value)//Esse comando vai pegar tudo que esta dentro dos inputs
+
+  }
 }
--- angulaaar/frontEnd/src/app/app.module.ts
+++ angulaaar/frontEnd/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from "@angular/forms"; //Preciso importar essa classe para poder pegar dados dos inputs 
+import { HttpClientModule } from "@angular/common/http";//`Precisamos importar esta classe, para podermos usar os métodos http do angular
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
