6489a52e371660d9c88908a898bcc04a13c6af93
Bruno Bandeira <brunodev29ab@gmail.com>
2021-05-25 13:39:54 -0300

    Json

--- angulaaar/frontEnd/src/app/app.component.ts
+++ angulaaar/frontEnd/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from "@angular/forms";//O FormGroup cria  um objeto que representa os campos dos formularios
+import { Todo } from "./todo";
+import { TodoService } from "./todo.service";
 
 @Component({
   selector: 'app-root',
@@ -11,8 +13,15 @@ export class AppComponent {
     description: new FormControl("")//OBS: como é um objeto JavaScript, você coloca :
   })
 
+  constructor(
+    private service: TodoService
+  ) {}
+
+  
   submit(){
     console.log(this.form.value)//Esse comando vai pegar tudo que esta dentro dos inputs
+    const todo: Todo = {...this.form.value }
+    this.service.salvar(todo).subscribe(savedTodo => console.log(savedTodo))
 
   }
 }
